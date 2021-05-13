1500861928dbd831d1f04484857a3db483da1e74
[бот] <[]@users.noreply.github.com>
2021-05-13 16:16:29 +0000

    Markdown > HTML

--- Сгенерированные страницы/___res/TagTable.js
+++ Сгенерированные страницы/___res/TagTable.js
@@ -12,5 +12,5 @@ class Article
 // Хеш-таблица тег -> массив статей
 
 let tagTable = {
-	'нет' : [new Article('Тест', 'Тест.md.html'), ],
+	'нет' : [new Article('Тест', 'Тест.md.html'), new Article('Тест 2', 'Тест%202.md.html'), ],
 };
